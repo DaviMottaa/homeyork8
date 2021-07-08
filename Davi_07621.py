233f97752cba6f10a5c3d730b8926fcdddc5c91a
DaviMottaa <84351196+DaviMottaa@users.noreply.github.com>
2021-07-07 23:09:10 -0300

    Add files via upload

---
+++ Davi_07621.py
@@ -0,0 +1,36 @@
+#Question2
+numbers = list(range(10,18))
+
+#Question3
+print("3****************************")
+print(numbers)
+
+#Question4
+print("4****************************")
+print(numbers[2:6])
+
+#Question5
+print("5****************************")
+print(numbers[1:8:2])
+
+#Question6
+print("6****************************")
+print(numbers[7:3:-1])
+
+#Question7
+print("7****************************")
+print(numbers[6::-3])
+
+#Question8
+print("8****************************")
+Frutas = ["kiwi","ORANGE","mango","CHERRY"]
+print(Frutas)
+
+#Question9
+print("9****************************")
+for Fruta in Frutas:
+    if Fruta.upper == "ORANGE":
+        print(Fruta)
+    else:
+        print(Fruta.upper())
+    
